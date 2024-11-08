d489bf25604069e2804a54f698c679ecd3a47852
Jia <2045083228@qq.com>
2024-11-08 17:50:03 +0800

    修改个人主页

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+    "./src/**/*.{html,js,jsx,ts,tsx}", // 包括所有的 HTML 和 JS 文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
